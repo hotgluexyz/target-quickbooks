c362b17acfdbc847ba7c7ac32c796ee0e864854f
Hassan Syyid <hassansyyid@gmail.com>
2021-03-22 16:42:01 -0400

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='target-quickbooks',
-    version='1.0.6',
+    version='1.0.7',
     description='hotglue target for exporting data to Quickbooks API',
     author='hotglue',
     url='https://hotglue.xyz',
