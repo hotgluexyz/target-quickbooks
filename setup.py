99d5462a625de430831fb2b9850ceb824d956129
Josué M. Sehnem <josue@sehnem.com>
2022-02-14 16:55:24 -0500

    new targets

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='target-quickbooks',
-    version='1.0.10',
+    version='1.0.11',
     description='hotglue target for exporting data to Quickbooks API',
     author='hotglue',
     url='https://hotglue.xyz',
